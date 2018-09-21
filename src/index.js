728be98fbf90d0825f7f580804e137eeb1dc887e
Arsal Mehran <arsalmehran@gmail.com>
2018-09-21 16:04:14 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import App from "./containers/App"
+import './containers/App.css';
+
+
+
+import 'tachyons';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+        <App />, document.getElementById('root'));
+registerServiceWorker();
